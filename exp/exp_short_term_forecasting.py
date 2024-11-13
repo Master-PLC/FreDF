6ce9e60d94d96e5e38659e8f98ce8b3bbda47a8e
Master-PLC <18358716036@163.com>
2024-11-13 17:14:51 +0800

    fix bugs for stf

--- exp/exp_short_term_forecasting.py
+++ exp/exp_short_term_forecasting.py
@@ -23,8 +23,8 @@ warnings.filterwarnings('ignore')
 
 class Exp_Short_Term_Forecast(Exp_Basic):
     def __init__(self, args):
-        super(Exp_Short_Term_Forecast, self).__init__(args)
         self.seasonal_patterns = args.seasonal_patterns
+        super(Exp_Short_Term_Forecast, self).__init__(args)
 
     def _build_model(self):
         if self.args.data == 'm4':
