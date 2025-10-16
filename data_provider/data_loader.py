744f0cc6d02d06bee6a02481ae9056b8ae300010
Master-PLC <18358716036@163.com>
2025-10-16 14:52:54 +0800

    add PEMS

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -431,6 +431,75 @@ class Dataset_Custom(Dataset):
         return self.scaler.inverse_transform(data)
 
 
+class Dataset_PEMS(Dataset):
+    def __init__(
+        self, root_path, flag='train', size=None, features='S', data_path='ETTh1.csv',
+        target='OT', scale=True, timeenc=0, freq='h', add_noise=False, **kwargs
+    ):
+        # size [seq_len, label_len, pred_len]
+        # info
+        self.seq_len = size[0]
+        self.label_len = size[1]
+        self.pred_len = size[2]
+        # init
+        assert flag in ['train', 'test', 'val']
+        type_map = {'train': 0, 'val': 1, 'test': 2}
+        self.set_type = type_map[flag]
+
+        self.features = features
+        self.target = target
+        self.scale = scale
+        self.timeenc = timeenc
+        self.freq = freq
+        self.add_noise = add_noise
+
+        self.root_path = root_path
+        self.data_path = data_path
+        self.__read_data__()
+
+    def __read_data__(self):
+        self.scaler = StandardScaler()
+        data_file = os.path.join(self.root_path, self.data_path)
+        data = np.load(data_file, allow_pickle=True)
+        data = data['data'][:, :, 0]
+
+        train_ratio = 0.6
+        valid_ratio = 0.2
+        border1s = [0, int(train_ratio * len(data)), int((train_ratio + valid_ratio) * len(data))]
+        border2s = [int(train_ratio * len(data)), int((train_ratio + valid_ratio) * len(data)), len(data)]
+        border1 = border1s[self.set_type]
+        border2 = border2s[self.set_type]
+
+        if self.scale:
+            self.scaler.fit(data[border1s[0]:border2s[0]])
+            data = self.scaler.transform(data)
+
+        df = pd.DataFrame(data)
+        data = df.fillna(method='ffill', limit=len(df)).fillna(method='bfill', limit=len(df)).values
+
+        self.data_x = data[border1:border2]
+        self.data_y = data[border1:border2]
+
+    def __getitem__(self, index):
+        s_begin = index
+        s_end = s_begin + self.seq_len
+        r_begin = s_end - self.label_len
+        r_end = s_end + self.pred_len
+
+        seq_x = self.data_x[s_begin:s_end]
+        seq_y = self.data_y[r_begin:r_end]
+        seq_x_mark = torch.zeros((seq_x.shape[0], 1))
+        seq_y_mark = torch.zeros((seq_y.shape[0], 1))
+
+        return seq_x, seq_y, seq_x_mark, seq_y_mark
+
+    def __len__(self):
+        return len(self.data_x) - self.seq_len - self.pred_len + 1
+
+    def inverse_transform(self, data):
+        return self.scaler.inverse_transform(data)
+
+
 class Dataset_M4(Dataset):
     def __init__(
         self, root_path, flag='pred', size=None, features='S', data_path='ETTh1.csv',
